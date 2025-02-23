edbda000767766e25e3d377b7b9100078cf82bdb
KamaTAEWOO <zszs2020@naver.com>
2025-02-23 17:40:10 +0900

    [feature] #10 remove hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
 }
 
@@ -17,11 +16,9 @@ dependencies {
     implementation(project(":feature"))
     implementation(project(":common"))
 
-    implementation(libs.hilt.android)
-    ksp(libs.hilt.android.compiler)
-    implementation(libs.hilt.navigation.compose)
-
     implementation(libs.timber)
+
+    implementation(libs.androidx.navigation.runtime.ktx)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
