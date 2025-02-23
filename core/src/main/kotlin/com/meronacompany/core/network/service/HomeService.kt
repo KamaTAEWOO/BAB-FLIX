d42a7f772cf254c8fe05b83cdb9c5d8748424eb0
KamaTAEWOO <zszs2020@naver.com>
2025-02-23 21:19:24 +0900

    [feature] #10 key 삭제 및 재발급

--- core/src/main/kotlin/com/meronacompany/core/network/service/HomeService.kt
+++ core/src/main/kotlin/com/meronacompany/core/network/service/HomeService.kt
@@ -1,7 +1,6 @@
 package com.meronacompany.core.network.service
 
 import com.meronacompany.core.model.dto.CommonDto
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
 
@@ -10,7 +9,7 @@ interface HomeService {
     @GET("authentication")
     suspend fun requestIsApiKey(
         @Header("Accept") accept: String = "application/json",
-        @Header("Authorization") authorization: String = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NTNkMTg2MzFjNGMxZGExZmEyNjA5MTNhMjIzNTczMyIsIm5iZiI6MTczOTcxMzMxNC44MzUsInN1YiI6IjY3YjFlYjIyNzI5NGJlOThhOGUwZGViNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3jzsvFYt-H8gkv3CAF3qBIjwf3sUrq8V5DNqTGfe04o"
+        @Header("Authorization") authorization: String = "Bearer "
     ): CommonDto
 
 }
