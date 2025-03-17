5a22fb8d95c1054ad7df6c5091232a62fa5e69c2
KamaTAEWOO <zszs2020@naver.com>
2025-03-17 23:07:40 +0900

    [feature] #52 movie 데이터 정렬 완료

--- feature/src/main/kotlin/com/meronacompany/feature/home/HomeScreen.kt
+++ feature/src/main/kotlin/com/meronacompany/feature/home/HomeScreen.kt
@@ -1,11 +1,13 @@
 package com.meronacompany.feature.home
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
@@ -54,14 +56,16 @@ fun ImageError(
     color: Color = Color.Gray,
     strokeWidth: Float = 8f
 ) {
-    Canvas(modifier = modifier) {
-        val size = size.minDimension
-        val startOffset1 = Offset(0f, 0f)
-        val endOffset1 = Offset(size, size)
-        val startOffset2 = Offset(size, 0f)
-        val endOffset2 = Offset(0f, size)
+    Box(modifier = modifier.fillMaxSize()) {
+        Canvas(modifier = Modifier.align(Alignment.Center).then(modifier)) {
+            val size = size.minDimension
+            val startOffset1 = Offset(0f, 0f)
+            val endOffset1 = Offset(size, size)
+            val startOffset2 = Offset(size, 0f)
+            val endOffset2 = Offset(0f, size)
 
-        drawLine(color = color, start = startOffset1, end = endOffset1, strokeWidth = strokeWidth)
-        drawLine(color = color, start = startOffset2, end = endOffset2, strokeWidth = strokeWidth)
+            drawLine(color = color, start = startOffset1, end = endOffset1, strokeWidth = strokeWidth)
+            drawLine(color = color, start = startOffset2, end = endOffset2, strokeWidth = strokeWidth)
+        }
     }
 }
