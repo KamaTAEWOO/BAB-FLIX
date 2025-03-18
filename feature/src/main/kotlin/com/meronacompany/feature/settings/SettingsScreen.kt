be0203277d5ef58bfe777cc78c06758c9f3633ab
KamaTAEWOO <zszs2020@naver.com>
2025-03-18 21:36:35 +0900

    [feature] #58 setting 화면 개발 준비

--- feature/src/main/kotlin/com/meronacompany/feature/settings/SettingsScreen.kt
+++ feature/src/main/kotlin/com/meronacompany/feature/settings/SettingsScreen.kt
@@ -7,6 +7,12 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import com.meronacompany.feature.navigation.bottom.BottomNavigationScreen
 
+/**
+ * 오픈 소스 라이브러리
+ * About
+ * 앱 종료
+ */
+
 @Composable
 fun SettingsScreen(navHostController: NavHostController) {
     Scaffold(
