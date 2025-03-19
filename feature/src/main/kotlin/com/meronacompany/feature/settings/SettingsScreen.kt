582d64ad144fe2f3a9e4160a1515eb741ef6a212
KamaTAEWOO <zszs2020@naver.com>
2025-03-19 22:26:43 +0900

    [feature] #58 앱정보 추가

--- feature/src/main/kotlin/com/meronacompany/feature/settings/SettingsScreen.kt
+++ feature/src/main/kotlin/com/meronacompany/feature/settings/SettingsScreen.kt
@@ -1,10 +1,16 @@
 package com.meronacompany.feature.settings
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import com.meronacompany.design.theme.BAB_FLIXTheme
 import com.meronacompany.feature.navigation.bottom.BottomNavigationScreen
 
 /**
@@ -15,17 +21,24 @@ import com.meronacompany.feature.navigation.bottom.BottomNavigationScreen
 
 @Composable
 fun SettingsScreen(navHostController: NavHostController) {
-    Scaffold(
-        topBar = {},
-        content = { paddingValues ->
-            SettingsContent(paddingValues)
-        },
-        bottomBar = { BottomNavigationScreen(navHostController) }
-    )
+    Scaffold(topBar = {}, content = { paddingValues ->
+        SettingsContent(paddingValues)
+    }, bottomBar = { BottomNavigationScreen(navHostController) })
 }
 
 @Composable
 fun SettingsContent(paddingValues: PaddingValues) {
-    // Detail Content
-    Text(text = "Settings Screen")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)
+    ) {
+        // 앱 정보
+        Text(
+            text = "Ver 1.0.0",
+            color = colorScheme.onPrimary,
+            style = BAB_FLIXTheme.typography.textStyleBold18
+        )
+    }
+
 }
